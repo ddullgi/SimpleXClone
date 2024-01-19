95c1104a1cd76fd48a6e92f49e06aee64a59cf1d
황상윤 <s_infinite@naver.com>
2024-01-19 12:17:33 +0900

    feat. start 페이지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      루트 레이아웃
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className={styles.container}>{children}</div>
+      </body>
     </html>
   );
 }
