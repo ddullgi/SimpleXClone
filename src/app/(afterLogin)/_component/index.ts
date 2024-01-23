cb4ec22481787420857962398be1407a41893247
황상윤 <s_infinite@naver.com>
2024-01-23 19:25:19 +0900

    feat. right menu 구현

---
+++ src/app/(afterLogin)/_component/index.ts
@@ -0,0 +1,5 @@
+export { default as NavMenu } from "./NavMenu";
+export { default as LogoutButton } from "./LogoutButton";
+export { default as Trend } from "./Trend";
+export { default as TrendSection } from "./TrendSection";
+export { default as FollowRecommend } from "./FollowRecommend";
